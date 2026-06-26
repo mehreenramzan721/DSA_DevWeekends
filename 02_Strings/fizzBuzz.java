abbcaad7c33b5b1ab2089b09989583df2011d35a
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-26 19:07:18 +0500

    Create fizzBuzz.java

---
+++ 02_Strings/fizzBuzz.java
@@ -0,0 +1,24 @@
+/*Given an integer `n`, return *a string array* `answer` *(**1-indexed**) where*:
+- `answer[i] == "FizzBuzz"` if `i` is divisible by `3` and `5`.
+- `answer[i] == "Fizz"` if `i` is divisible by `3`.
+- `answer[i] == "Buzz"` if `i` is divisible by `5`.
+- `answer[i] == i` (as a string) if none of the above conditions are true.
+
+*/
+class fizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer=new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            }else if(i % 3 == 0) {
+                answer.add("Fizz");
+            }else if(i % 5 == 0) {
+                answer.add("Buzz");
+            }else{
+                answer.add(String.valueOf(i));
+            }
+        }
+        return answer;
+    }
+}
